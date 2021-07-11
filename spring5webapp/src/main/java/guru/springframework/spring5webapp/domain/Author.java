0b453baeaf24d607b364356963bf71fcabdd4a8e
kashsaeed <kashifnizami@gmail.com>
2021-07-11 18:24:19 +0200

    2nd part covered H2 DB & Thymeleaf, MVC

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -67,6 +67,15 @@ public class Author {
     }
 
     @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
     public int hashCode() {
         return Objects.hash(id);
     }
